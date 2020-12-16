d6dbc9124f86add8335c7060974db6e9f865da4c
Sajjad Ahmad <jogimar14@gmail.com>
2020-12-16 13:33:02 +0500

    update destroy action to receive friend_id

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,6 +5,7 @@ class FriendshipsController < ApplicationController
   def create
     friend_id = params[:friend_id]
     @friendship = Friendship.find_or_create_by(user_id: current_user.id, friend_id: friend_id, confirmed: false)
+    redirect_to users_path
   end
 
   def update
@@ -18,10 +19,8 @@ class FriendshipsController < ApplicationController
   end
 
   def destroy
-    # user = User.find(params[:user_id])
-    # friend = User.find(params[:friend_id])
-    # Friendship.find_by_user_id_and_friend_id(user, friend).destroy
-    # Friendship.find_by_user_id_and_friend_id(friend, user).destroy
-    # redirect_to users_path
+    user = User.find(params[:user_id])
+    Friendship.find_by_friend_id(user.id).destroy
+    redirect_to users_path
   end
 end
