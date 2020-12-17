f44ede6d3205756315f7388baadadaee1914ec28
Sajjad Ahmad <jogimar14@gmail.com>
2020-12-17 14:21:52 +0500

    add feature specs for send an invite, cancel pending invite, accept a request and reject a request

---
+++ spec/feature/friendship_feature_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+RSpec.feature 'Friendship request handling', type: :feature do
+  before(:each) do
+    @a = User.create(name: 'a', email: 'a@a.com', password: '123456')
+    @b = User.create(name: 'b', email: 'b@b.com', password: '123456')
+  end
+  scenario 'invite a user for friendship' do
+    visit 'users/sign_in'
+    fill_in 'user[email]',	with: 'a@a.com'
+    fill_in 'user[password]',	with: '123456'
+    click_button 'Log in'
+    visit 'users'
+    click_button 'Invite'
+    expect(page).to have_text("Invitation sent successfully!")
+  end
+  scenario 'Cancel pending invitation' do
+    visit 'users/sign_in'
+    fill_in 'user[email]',	with: 'a@a.com'
+    fill_in 'user[password]',	with: '123456'
+    click_button 'Log in'
+    visit 'users'
+    click_button 'Invite'
+    click_button 'Cancel'
+    visit 'users'
+  end
+  scenario 'Accept an invitation' do
+    visit 'users/sign_in'
+    fill_in 'user[email]',	with: 'a@a.com'
+    fill_in 'user[password]',	with: '123456'
+    click_button 'Log in'
+    visit 'users'
+    click_button 'Invite'
+    click_on 'Sign out'
+    visit new_user_session_path
+    fill_in 'user[email]',	with: 'b@b.com'
+    fill_in 'user[password]',	with: '123456'
+    click_button 'Log in'
+    visit 'users'
+    expect(page).to have_text('a')
+    click_button 'Accept'
+    visit 'users'
+  end
+  scenario 'Reject an invitation' do
+    visit 'users/sign_in'
+    fill_in 'user[email]',	with: 'a@a.com'
+    fill_in 'user[password]',	with: '123456'
+    click_button 'Log in'
+    visit 'users'
+    click_button 'Invite'
+    click_on 'Sign out'
+    visit new_user_session_path
+    fill_in 'user[email]',	with: 'b@b.com'
+    fill_in 'user[password]',	with: '123456'
+    click_button 'Log in'
+    visit 'users'
+    expect(page).to have_text('a')
+    click_button 'Reject'
+    visit 'users'
+  end
+end
