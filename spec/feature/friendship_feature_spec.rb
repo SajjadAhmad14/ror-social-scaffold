266c3fcbab834f6b6d655b408d86c6609f94491a
Sajjad Ahmad <jogimar14@gmail.com>
2020-12-17 14:30:28 +0500

    fix stylelint and rubocop errors

--- spec/feature/friendship_feature_spec.rb
+++ spec/feature/friendship_feature_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-RSpec.feature 'Friendship request handling', type: :feature do
+RSpec.feature 'Friendship invitation', type: :feature do
   before(:each) do
     @a = User.create(name: 'a', email: 'a@a.com', password: '123456')
     @b = User.create(name: 'b', email: 'b@b.com', password: '123456')
@@ -11,7 +11,13 @@ RSpec.feature 'Friendship request handling', type: :feature do
     click_button 'Log in'
     visit 'users'
     click_button 'Invite'
-    expect(page).to have_text("Invitation sent successfully!")
+    expect(page).to have_text('Invitation sent successfully!')
+  end
+end
+RSpec.feature 'Invitation cancelation', type: :feature do
+  before(:each) do
+    @a = User.create(name: 'a', email: 'a@a.com', password: '123456')
+    @b = User.create(name: 'b', email: 'b@b.com', password: '123456')
   end
   scenario 'Cancel pending invitation' do
     visit 'users/sign_in'
@@ -22,6 +28,12 @@ RSpec.feature 'Friendship request handling', type: :feature do
     click_button 'Invite'
     click_button 'Cancel'
     visit 'users'
+  end
+end
+RSpec.feature 'Invitation acceptence', type: :feature do
+  before(:each) do
+    @a = User.create(name: 'a', email: 'a@a.com', password: '123456')
+    @b = User.create(name: 'b', email: 'b@b.com', password: '123456')
   end
   scenario 'Accept an invitation' do
     visit 'users/sign_in'
@@ -39,6 +51,12 @@ RSpec.feature 'Friendship request handling', type: :feature do
     expect(page).to have_text('a')
     click_button 'Accept'
     visit 'users'
+  end
+end
+RSpec.feature 'Invitation Rejection', type: :feature do
+  before(:each) do
+    @a = User.create(name: 'a', email: 'a@a.com', password: '123456')
+    @b = User.create(name: 'b', email: 'b@b.com', password: '123456')
   end
   scenario 'Reject an invitation' do
     visit 'users/sign_in'
